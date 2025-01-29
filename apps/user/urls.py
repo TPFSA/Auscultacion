1fddb1dff049d181fd7831a886b1a1147e9773e5
AlinaTPF <alina.hernandez@tpfingenieria.com>
2025-01-29 11:44:46 +0100

    First commit

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
-from apps.login import views
+from apps.user import views
 
 urlpatterns = [
     path('login/', views.login),
     path('signup/', views.signup),
+    path('profile/', views.profile),
 ]
